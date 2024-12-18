50f0a8e20bf6b7c9327ef52d62f629ea8bd59b49
teaneziri <neziritea@gmail.com>
2024-12-18 12:31:06 +0100

    replaced href with Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from "../assets/images/logo.png"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -23,21 +24,19 @@ const Navbar = () => {
             </a>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
+                <Link
+                  to="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
-                <a
-                  href="/jobs.html"
+                  >Home</Link>
+                <Link
+                  to="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
-                <a
-                  href="/add-job.html"
+                  >Jobs</Link>
+                <Link
+                  to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
+                  >Add Job
+                </Link>
               </div>
             </div>
           </div>
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const JobsPage = () => {
+  return (
+    <>
+   <h1>Your are on Jobs page</h1>
+    </>
+  )
+}
+
+export default JobsPage
