446a1641b6cb4ba166ce0a63911b0eac7d6b721d
teaneziri <neziritea@gmail.com>
2024-12-18 14:40:19 +0100

    fetched data

--- src/components/JobsListings.jsx
+++ src/components/JobsListings.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import jobs from "../jobs.json"
 import JobListing from './JobListing'
+import Spinner from './Spinner'
+
 
 const JobsListings = ({isHome=false}) => {
-    const jobListings=isHome ? jobs.slice(0,3) : jobs;
+   const [jobs, setJobs]=useState([]);
+   const [loading, setLoading]=useState(true)
+
+   useEffect(()=>{
+     const fetchJobs=async()=>{
+      try{
+        const res=await fetch('http://localhost:8000/jobs');
+        const data=await res.json();
+        setJobs(data)
+      }catch(error){
+        console.log('Error fetching data ', error)
+      }finally{
+        setLoading(false)
+      }
+     }
+
+     fetchJobs()
+   },[])
+
   return (
     <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
@@ -11,9 +31,13 @@ const JobsListings = ({isHome=false}) => {
        {isHome ? 'Recent Jobs' : 'Browse Jobs'}
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {jobListings.map((job)=>(
+       {loading ? (<Spinner loading={loading} />) : (
+        <>
+         {jobs.map((job)=>(
             <JobListing key={job.id} job={job}/>
         ))}
+        </>
+       )}
         
       </div>
     </div>
