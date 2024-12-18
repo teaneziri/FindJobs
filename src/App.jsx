20d7ac1f0e2c84fd122168ac783948d8f91d8d73
teaneziri <neziritea@gmail.com>
2024-12-18 11:27:30 +0100

    Some little changes on cards details

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
 import JobsListings from "./components/JobsListings"
+import ViewAllJobs from "./components/ViewAllJobs"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
      <Hero title='Become a React Dev' subtitle="Find the React job that fits your skills and needs"/>
      <HomeCards />
      <JobsListings />
+     <ViewAllJobs />
     </>
   )
 }
