5482bebc9aa04a7af9cc879e3ef1f2cb2bea3e95
teaneziri <neziritea@gmail.com>
2024-12-18 14:55:24 +0100

    some little changes

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'http://lcalhost:8000',
+        target:'http://localhost:8000',
         changeOrigin:true,
-        rewrite:(path)=>path.replace(/^\api/,'')
+        rewrite:(path)=>path.replace(/^\/api/,'')
       }
     }
   }
