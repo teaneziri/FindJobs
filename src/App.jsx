37e835ef31c18ec4e5243168d015fa47e1a20bcd
teaneziri <neziritea@gmail.com>
2024-12-18 22:39:37 +0100

    Delete function finished

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,20 @@ const App = () => {
     return;
   };
 
+  //Delete Job
+  const deleteJob=async(id)=>{
+    const res=await fetch(`/api/jobs/${id}`,{
+      method:'DELETE'
+    })
+    return
+  }
 
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader}/>
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />}/>
         <Route path='*' element={<NotFoundPage />}/>
         
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 
+
+
 const MainLayout = () => {
   return (
     <>
